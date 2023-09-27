07e9ee8d9cbbcf09d1dcd4a55f6f333c8ea18b45
Jemin Song <jeminsong0119@csu.fullerton.edu>
2023-09-27 01:15:49 -0700

    Checking.

--- part-2/datediff.cc
+++ part-2/datediff.cc
@@ -1,66 +1,49 @@
-// TODO: add a header
+// Jemin Song
+// jeminsong0119@csu.fullerton.edu
+// @jeminsong
+// Partners: @Devon-T928
 
 #include <iostream>
 
-// Given a month, day, and year, calculate the Julian day number using the
-// Fliegel & Van Flandern equation.
-int JulianDay(int month, int day, int year) {
-  // TODO: write statements to implement this function, and delete this comment
-
-  // TODO: replace this return statement with one that actually works
-  return -1;
+int julian_day(int month, int day, int year) {
+ int mon = (14-month) / 12;
+ int yar = year + 4800 - mon;
+ int mos = month + (12 * mon) - 3;
+ int day_term = day + (153 * mos + 2) / 5;
+ int year_term = 365 * yar + yar / 4 - yar / 100 + yar / 400;
+ int jdate = day_term + year_term - 32045;
+ return jdate;
 }
-
-// Return the number of days between a start date and end date.
 int DateDifference(int start_month, int start_day, int start_year,
                    int end_month, int end_day, int end_year) {
-  // TODO: write statements to implement this function, and delete this comment
-
-  // TODO: replace this return statement with one that actually works
-  return -1;
+ int start_jd = julian_day(start_month, start_day, start_year);
+ int end_jd = julian_day(end_month, end_day, end_year);
+ return end_jd - start_jd;
 }
 
-// Print the given date to standard output in the format MM/DD/YYYY
 void PrintDate(int month, int day, int year) {
-  // TODO: write statements to implement this function, and delete this comment
+  std::cout << month << "/" << day << "/" << year;
 }
-
-// Prompt the computer with a question, capture their answer, and return it to
-// the caller
-int PromptForInt(std::string query) {
-  // TODO: write statements to implement this function, and delete this comment
-  // Hint: On Canvas, find the sample code named cpsc120_prompt.cc
-
-  // TODO: replace this return statement with one that actually works
-  return -1;
+int PromptForInt(const std::string& query) {
+  int input = 0;
+  std::cout << query << " ";
+  std::cin >> input;
+  return input;
 }
-
 int main(int argc, char* argv[]) {
-  // TODO: Declare and initalize an integer variable named start_month, using
-  // the function PromptForInt. The prompt must be "Enter a start month: "
-  // TODO: Declare and initalize an integer variable named start_day, using the
-  // function PromptForInt. The prompt must be "Enter a start day: "
-  // TODO: Declare and initalize an integer variable named start_year, using the
-  // function PromptForInt. The prompt must be "Enter a start year: "
-
-  // TODO: Declare and initalize an integer variable named end_month, using the
-  // function PromptForInt. The prompt must be "Enter an end month: "
-  // TODO: Declare and initalize an integer variable named end_day, using the
-  // function PromptForInt. The prompt must be "Enter an end day: "
-  // TODO: Declare and initalize an integer variable named end_year, using the
-  // function PromptForInt. The prompt must be "Enter an end year: "
-
-  // TODO: Declare an initalize an integer variable named difference, using the
-  // function DateDifference.
-
-  // TODO: Using std::cout, output the string "The number of days between"
-  // TODO: Call PrintDate to print the start date
-  // TODO: Using std::cout, output the string " and "
-  // TODO: Call PrintDate to print the end date
-  // TODO: Using std::cout, output the string " is "
-  // TODO: Using std::cout, output the value of the variable named difference
-  // (which is the difference calculated between our start date and end date).
-  // TODO: Using std::cout, output the string " days.\n"
-
-  return 0;
+ int start_month = PromptForInt("Enter a start_month: ");
+ int start_day = PromptForInt("Enter a start date: ");
+ int start_year = PromptForInt("Enter a start year: ");
+ int end_month = PromptForInt("Enter an end month: ");
+ int end_day = PromptForInt("Enter an end day: ");
+ int end_year = PromptForInt("Enter an end year: "); 
+ int difference = DateDifference (
+     start_month, start_day, start_year,
+     end_month, end_day, end_year);
+ std::cout << "The number of days between ";
+ PrintDate(start_month, start_day, start_year);
+ std::cout << " and ";
+ PrintDate(end_month, end_day, end_year);
+ std::cout << " is " << difference << " days." << ".\n";
+ return 0;
 }
