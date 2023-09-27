3000f6d97b079c04e43e1226698f16adea8a79b8
Jemin Song <jeminsong0119@csu.fullerton.edu>
2023-09-26 19:32:16 -0700

    Part-1 Lab03 Commit.

--- part-1/pizza.cc
+++ part-1/pizza.cc
@@ -1,31 +1,24 @@
-// TODO: add a header
+// Jemin Song
+// jeminsong0119@csu.fullerton.edu
+// @jeminsong
+// Partners: @Devon-T928
 
 #include <iostream>
-
-// Given the diameter and price of a pizza, calculate the unit price, which is
-// the number of dollars per square inch.
 double PizzaUnitPrice(double diameter, double price) {
-  // TODO: write statements to implement this function, and delete this comment
-  return 0;  // TODO: replace this return statement with one that actually works
+  double radius{diameter / 2};
+  double area = (3.1415926) * (radius * radius);
+  double unit_price = (price / area);
+  return unit_price;
 }
 
-int main(int argc, char* argv[]) {
-  // TODO: Declare a variable to hold pizza diameter;
-  // print the prompt "Enter pizza diameter in inches: "; and
-  // input a number from standard input into the variable.
-
-  // TODO: Declare a variable to hold pizza price;
-  // print the prompt "Enter pizza price in dollars: "; and
-  // input a number from standard input into the variable.
-
-  // TODO: Declare a variable to hold unit price;
-  // initialize the variable with the value returned from a call to the
-  // PizzaUnitPrice function above.
-
-  // TODO: Print out "Unit price = ";
-  // then the unit price variable;
-  // then " dollars per square inch";
-  // then a newline character.
-
+int main() {
+  double diameter{0.0};
+  std::cout << "Enter pizza diameter in inches:";
+  std::cin >> diameter;
+  double price{0.0};
+  std::cout << "Enter pizza price in dollars:";
+  std::cin >> price;
+  double unit_price{PizzaUnitPrice(diameter, price)};
+  std::cout << "Unit price = " << unit_price << " dollars per square inch\n";
   return 0;
 }
